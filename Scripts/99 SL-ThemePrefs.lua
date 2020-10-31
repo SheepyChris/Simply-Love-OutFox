6e141cec0beb9b0612a65a0d9e6d61a2f8bdedd5
moruzerinho6 <zerinho6@gmail.com>
2020-10-31 18:43:47 -0300

    Adds TinyFoxes VisualStyle

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -75,8 +75,8 @@ SL_CustomPrefs.Get = function()
 		{
 			Default = "Hearts",
 			 -- emojis are our lingua franca for the 21st century
-			Choices = { "♡", "↖", "🐻", "🦆", "😺", "🎃", "🌈", "⭐", "🤔" },
-			Values  = { "Hearts", "Arrows", "Bears", "Ducks", "Cats", "Spooky", "Gay", "Stars", "Thonk" },
+			 Choices = { "♡", "↖", "🐻", "🦆", "😺", "🎃", "🌈", "⭐", "🤔", "TinyFoxes" },
+			 Values  = { "Hearts", "Arrows", "Bears", "Ducks", "Cats", "Spooky", "Gay", "Stars", "Thonk", "TinyFoxes" },
 		},
 		RainbowMode = {
 			Default = false,
